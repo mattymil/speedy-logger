a94484011a63c7f49bb0d6aaca6ff0c4420dfa2b
Matthew C. Miller <matt@digitalvecte.com>
2017-01-12 17:15:29 -0500

    cleaned up ; from code

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-let firebase = require('firebase');
+let firebase = require('firebase')
 let speedTest = require('speedtest-net')
-let config = require('./config.json');
-let dbConfig = config.dbConfig;
+let config = require('./config.json')
+let dbConfig = config.dbConfig
 
-firebase.initializeApp(dbConfig);
+firebase.initializeApp(dbConfig)
 
 // Get a reference to the database service
-const database = firebase.database();
+const database = firebase.database()
 
 // initialize the speedtest with the appropriate test time and pingCount
-const test = speedTest({maxTime: 20000});
+const test = speedTest({maxTime: 20000})
 
 // log results to the database
 test.on('data', data => {
-  database.ref('tests/' + Date.now()).set(data);
-  console.log(data);
-  process.exit();
+  //add a time tested property to the object
+  database.ref('tests/' + Date.now()).set(data)
+  process.exit()
 });
 
 // on error log it to the database
 test.on('error', err => {
-  database.ref('errors/' + Date.now()).set(error);
-  process.exit();
+  database.ref('errors/' + Date.now()).set(error)
+  process.exit()
 });
